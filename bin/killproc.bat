b11867cdf138a68c8622e6b2c2faa3cebc3e9af6
ymejias <ymejias@cyberfuel.com>
2025-10-29 11:01:07 -0600

    Actualización de los timeout en apache y nginx

--- bin/killproc.bat
+++ bin/killproc.bat
@@ -2,10 +2,10 @@
 setlocal
 set "proc=%1"
 set "dir=%2"
-if %proc% == "" (
+if "%proc%" == "" (
 	exit /b 1
 )
-if %dir% == "" (
+if "%dir%" == "" (
 	exit /b 1
 )
 powershell -Command "Get-Process %proc% | Where-Object { $_.Path -like '%dir%*' } | ForEach-Object { Stop-Process -Id $_.Id -Force }"
